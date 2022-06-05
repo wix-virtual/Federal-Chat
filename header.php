20cdb758e6fb55ff2874003fc0c47fbac1c0e456
Farhan Ali Hyder <106154544+wix-virtual@users.noreply.github.com>
2022-06-06 01:04:00 +0530

    Initial Commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index,follow">
+    <meta name="googlebot" content="index">
+    <meta name="author" content="Federal Group">
+
+    <!-- Font Awesome CDN, Header Icon and CSS File Links -->
+    <link rel="icon" href="logo/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <title>Federal Chat</title>
+</head>
